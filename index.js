f73fc3f4dbf41a2cfc8308aa299c23cf2a0f3855
Saveliy <svolskey2929@gmail.com>
2022-10-02 17:21:04 +0300

    add auth

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import mongoose from "mongoose";
-import {validationResult} from "express-validator";
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import {registerValidation} from "./validations/auth.js";
 
-import UserModel from './models/User.js'
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
   .connect('mongodb+srv://admin:Qwerty1233@cluster0.mo1afw6.mongodb.net/blog?retryWrites=true&w=majority')
@@ -29,40 +27,8 @@ app.get('/', (request, response) => {
 })
 
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
-    }
-    
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
-    
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash
-    })
-    
-    const user = await doc.save()
-    
-    const token = jwt.sign({
-      _id: user._id,
-    }, 'secret123', {
-      expiresIn: '30d'
-    })
-    
-    res.json({
-      user,
-      token
-    })
-  } catch(e){
-    console.log(e)
-    res.status(500).json({
-      message: 'Не удалось зарегистрироваться'
-    })
-  }
-})
+app.post('/auth/register', registerValidation, UserController.register)
+
+app.post('/auth/login', UserController.login)
+
+app.get('/auth/me', checkAuth, UserController.getMe)
